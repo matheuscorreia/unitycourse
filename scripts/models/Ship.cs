e69881390560e1cc91c6ff286144f3ef5448c84e
matheuscorreia <matheuscorreia2005@gmail.com>
2017-06-11 13:45:37 -0300

    Redoing everything

---
+++ scripts/models/Ship.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Models {
+
+	public abstract class Ship : MonoBehaviour {
+
+		//	attributes
+		protected float maxHitPoints;
+		protected float currentHitPoints;
+		protected float baseDamage;
+		protected float baseDefense;
+
+		//	gameObject
+		protected GameObject gmob;
+
+		//	components
+		protected Rigidbody2D rb;
+		protected BoxCollider2D bc;
+		protected SpriteRenderer sr;
+
+		protected abstract void Shoot();
+
+		protected abstract void Move();
+
+		protected void Initialize(){
+			//	Setting game object
+			this.gmob = this.gameObject;
+
+			//	Initializing components
+			this.rb = this.gmob.GetComponent<Rigidbody2D>();
+			this.bc = this.gmob.GetComponent<BoxCollider2D>();
+			this.sr = this.gmob.GetComponent<SpriteRenderer>();
+		}
+
+	}
+
+}
---
+++ scripts/objects/PlayerShip.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using Models;
+
+public class PlayerShip : Models.Ship {
+
+	protected Sprite sprite;
+
+	public void Awake(){
+		this.Initialize();
+
+		PlayerPrefs.SetString("ShipSprite", "Assets/Resources/sprites/ships/player/red/player1.png");
+
+		this.sprite = (Sprite) Resources.Load(PlayerPrefs.GetString("ShipSprite"), typeof(Sprite));
+		this.sr.sprite = this.sprite;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	protected override void Shoot(){
+
+	}
+
+	protected override void Move(){
+
+	}
+}
